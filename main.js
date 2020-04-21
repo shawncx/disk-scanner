ef98db7182c1f6db57f190c28c4a40e32cfa7cdf
shawncx <braincx@163.com>
2020-04-21 12:07:23 +0800

    change pj name. Close devtool by default

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ function createWindow() {
     })
   );
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 
   mainWindow.on('closed', function () {
     mainWindow = null;
